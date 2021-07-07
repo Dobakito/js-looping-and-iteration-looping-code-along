0942b0e230b2d9c3739a63c2ec8e660c19bfbb20
Dobakito <mdobak44@icloud.com>
2021-07-06 19:49:36 -0500

    commit tree ed5f0a30bd7ef48cb85d8e6b1ba038653c2abede

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(ppl, word) {
+  let cards = []
+  for (let l = 0; l < ppl.length; l++) {
+    cards.push(`Thank you, ${ppl[l]}, for the wonderful ${word} gift!`);
+  }
+  return cards
+}
+
+function countDown(num){
+  let n = num ;
+  while (n >= 0) {
+    console.log(n--)
+  }
+}
